259672423976131d96a9e5e50e0ae7adf076bcb7
user name <user email>
2022-04-28 15:41:44 -0700

    React App w/ Guidance

---
+++ setup/src/App.js
@@ -0,0 +1,20 @@
+//  followed along to a youtube tutorial on how to build this application
+
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3> {people.length} Birthdays Today </h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
